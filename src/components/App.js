29562d87e59b8bd41791648fb43affc197e13b42
Sara McDonnell <saramcdonnell32@gmail.com>
2023-07-24 19:14:38 -0400

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
+import { useState, useEffect } from "react";
+
 // create your App component here
+
+function App() {
+  const [pic, setPic] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random").then((resp) =>
+      resp.json().then((data) => setPic(data.message))
+    );
+  }, []);
+
+  if (!pic) {
+    return <p>Loading...</p>;
+  }
+  return <img src={pic} alt="A Random Dog" />;
+}
+
+export default App;
